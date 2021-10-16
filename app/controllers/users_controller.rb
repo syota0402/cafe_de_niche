ceb224dc74f0a3d4a9617b845021c0deaaa4e3a3
syota <syota.3y5s@gmail.com>
2021-10-16 07:11:23 +0000

    プロフィールの変更機能の追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  before_action :set_user
+  
+  def edit
+  end
+
+  def update
+    @user.update_without_password(user_params)
+    redirect_to root_path 
+  end
+
+  def mypage
+  end
+  
+  private
+  def set_user
+    @user = current_user
+  end
+  
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/update'
+  get 'users/mypage'
   get "dashboard", :to => "dashboard#index"
   
   resources :coffee_shops do
@@ -24,6 +27,14 @@ Rails.application.routes.draw do
     get "verify", :to => "users/registrations#verify"
     get "login", :to => "users/sessions#new"
     delete "logout", :to => "users/sessions#destroy"
+  end
+  
+  resources :users, only: [:edit, :update] do
+    collection do
+      get "mypage", :to => "users#mypage"
+      get "mypage/edit", :to => "users#edit"
+      put "mypage", :to => "users#update"
+    end
   end
   
 end
