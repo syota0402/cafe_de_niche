03420559d09171115b335e71abb1f6dcf3f649e1
syota <syota.3y5s@gmail.com>
2021-10-17 05:00:51 +0000

    パスワード変更機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,18 @@ class UsersController < ApplicationController
   def mypage
   end
   
+  def update_password
+    if password_set? && @user.update_password(user_params)
+      flash[:notice] = "パスワードは正しく更新されました"
+      redirect_to root_path
+    else
+      @user.errors.add(:password, "パスワードに不備があります")
+      render "edit_password"
+    end
+  end
   
+  def edit_password
+  end
   
   private
   def set_user
@@ -21,5 +32,10 @@ class UsersController < ApplicationController
   
   def user_params
     params.permit(:name, :email, :password, :password_confirmation, :age, :gender, :self_introduction, :image, :instagram_url)
+  end
+  
+  def password_set?
+    user_params[:password].present? && user_params[:password_confirmation].present? ?
+    true : false
   end
 end
