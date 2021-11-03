3970e9f7fbae4445e2f8947b6a7164dfc14ccb57
syota <syota.3y5s@gmail.com>
2021-11-03 05:38:48 +0000

    レビューの画像機能を追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -20,6 +20,6 @@ class ReviewsController < ApplicationController
   
   private
   def review_params
-    params.require(:review).permit(:review_comment, :review_score).merge(user_id: current_user.id, coffee_shop_id: params[:coffee_shop_id])
+    params.require(:review).permit(:review_comment, :review_score, :image).merge(user_id: current_user.id, coffee_shop_id: params[:coffee_shop_id])
   end
 end
