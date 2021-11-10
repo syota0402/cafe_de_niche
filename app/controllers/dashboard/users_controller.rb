220e0d0312021c3c0574c2a7071fa03f75a6a970
syota <syota.3y5s@gmail.com>
2021-11-10 11:53:00 +0000

    管理画面が対象者のみ表示されるように変更

--- app/controllers/dashboard/users_controller.rb
+++ app/controllers/dashboard/users_controller.rb
@@ -1,4 +1,5 @@
 class Dashboard::UsersController < ApplicationController
+  before_action :set_user, only: %w[edit update destroy]
   layout "dashboard/dashboard"
   
   def index
@@ -11,25 +12,25 @@ class Dashboard::UsersController < ApplicationController
   end
   
   def destroy
-    user = User.find(params[:id])
-    deleted_flg = User.switch_flg(user.deleted_flg)
-    user.update(deleted_flg: deleted_flg)
+    deleted_flg = User.switch_flg(@user.deleted_flg)
+    @user.update(deleted_flg: deleted_flg)
     redirect_to dashboard_users_path
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to dashboard_users_path
   end
   
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
   def user_params
-    # params.require(:user).permit(:authority)
     params.permit(:authority)
   end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,21 @@
 class DashboardController < ApplicationController
+	before_action :check_user_authority, only: %w[index]
 	layout 'dashboard/dashboard'
 	
 	def index
-		
+	end
+	
+	private
+	def check_user_authority
+		@user = current_user
+		if @user.authority.nil?
+			flash[:alert] = "権限がありません"
+			redirect_to root_path
+		else
+			if @user.authority < "1"
+				flash[:alert] = "権限がありません"
+				redirect_to root_path 
+			end
+		end
 	end
 end
