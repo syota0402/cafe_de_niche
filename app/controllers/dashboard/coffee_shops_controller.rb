d46fed2c5453030b1fe51b4644377799dadcd13d
syota <syota.3y5s@gmail.com>
2021-11-17 12:24:32 +0000

    年齢層登録機能追加

--- app/controllers/dashboard/coffee_shops_controller.rb
+++ app/controllers/dashboard/coffee_shops_controller.rb
@@ -65,7 +65,7 @@ class Dashboard::CoffeeShopsController < ApplicationController
   end
   
   def coffee_shop_params
-    params.require(:coffee_shop).permit(:name, :shop_url, :address, :tell, :access, :business_start_hour, :business_end_hour, :instagram_url, :instagram_spot_url, :municipalitie_id, :slack_time_start, :slack_time_end, {:search_category_ids => []}, images: [])
+    params.require(:coffee_shop).permit(:name, :shop_url, :address, :tell, :access, :business_start_hour, :business_end_hour, :instagram_url, :instagram_spot_url, :municipalitie_id, :slack_time_start, :slack_time_end, :age_group, {:search_category_ids => []}, images: [])
   end
   
   def check_user_authority
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_112414) do
+ActiveRecord::Schema.define(version: 2021_11_17_121145) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 2021_11_17_112414) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "age_groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "coffee_shop_search_categories", force: :cascade do |t|
@@ -61,6 +67,7 @@ ActiveRecord::Schema.define(version: 2021_11_17_112414) do
     t.integer "likers_count", default: 0
     t.time "slack_time_start"
     t.time "slack_time_end"
+    t.string "age_group"
   end
 
   create_table "day_of_weeks", force: :cascade do |t|
